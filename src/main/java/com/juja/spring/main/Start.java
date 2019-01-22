77c7fc8105ef7eb160521bb98a81a3d777c2940c
sergey-kochetov <git.kochetov@gmail.com>
2019-01-22 14:12:17 +0300

    Spring context -> T1000Empty

--- src/main/java/com/juja/spring/main/Start.java
+++ src/main/java/com/juja/spring/main/Start.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Start {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
+        ModelT1000 t1000 = (ModelT1000) context.getBean("t1000Empty");
         t1000.action();
     }
 }
